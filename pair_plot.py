46cb164b9f537a722974c0cb5b9d8be6464d3fa5
Themancrazy <ajanssen@live.fr>
2020-02-01 17:48:16 -0800

    pair plot matrice done

---
+++ pair_plot.py
@@ -0,0 +1,19 @@
+import sys
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+def makeScattering():
+	file = open('resources/dataset_train.csv')
+	df = pd.read_csv(file)
+	# df = df.loc[:, ("Hogwarts House", "Astronomy", "Herbology")]
+	df = df.dropna(how='any', axis=0)
+	sns.pairplot(df, hue="Hogwarts House")
+	plt.show()
+
+if __name__ == "__main__":
+	if len(sys.argv) != 1:
+		print("scatter doesn't take arguments.")
+		exit(1)
+	makeScattering()   
