91a196b59ce42ee46d2993fb28210af50c5e80ca
Themancrazy <ajanssen@live.fr>
2020-02-03 09:16:46 -0800

    pushed intro predict

---
+++ logreg_predict.py
@@ -0,0 +1,12 @@
+import sys
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+def predict():
+	"""Get weights
+	"""
+
+if __name__ == "__main__":
+   predictClass() 
